ae28877f6eeb8c879539288feb00b62c08e75df2
jimialex <jimialex06@gmail.com>
2014-08-19 23:27:24 -0400

    Initial commit

---
+++ app/src/main/java/jim/app/miscontactos/MyActivity.java
@@ -0,0 +1,18 @@
+package jim.app.miscontactos;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+
+}
